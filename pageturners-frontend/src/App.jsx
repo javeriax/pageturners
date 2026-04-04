45ce7b183599843efba0d630a6b55012257968b4
Ahla Haider <ahlahaider@Ahlas-MacBook-Air.local>
2026-04-04 15:50:50 +0500

    Integrate Login with Registration routes

--- pageturners-frontend/src/App.jsx
+++ pageturners-frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Register from './pages/Register';
+import Login from './pages/Login';
 
 // App: Main application component that sets up routing for different pages
 function App() {
@@ -11,6 +12,7 @@ function App() {
         {/* Routes defines which component to show based on URL path */}
         <Routes>
           {/* "/" path shows Register component - this is the main page */}
+          <Route path="/login" element={<Login />} />
           <Route path="/" element={<Register />} />
           {/* "/home" path shows Home component */}
           <Route path="/home" element={<Home />} />
